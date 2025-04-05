819092c4f34c0ba7c5c7c60650cb00fab793c555
Luc <luc001@e.ntu.edu.sg>
2025-04-05 20:12:04 +0000

    Massive restructuring

---
+++ sam_thruster_relay/launch/relay_nodes.launch.py
@@ -0,0 +1,34 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    config = os.path.join(get_package_share_directory('sam_thruster_relay'), 'config', 'relay_params.yaml')
+
+    thrust_cmd_relay = Node(
+        name='thrust_cmd_relay',
+        executable='thrust_cmd_relay',
+        package='sam_thruster_relay',
+        parameters=[config]
+    )
+
+    thrust_vector_relay = Node(
+        name='thrust_vector_relay',
+        executable='thrust_vector_relay',
+        package='sam_thruster_relay',
+        parameters=[config]
+    )
+
+    thrust_vector_horizontal_relay = Node(
+        name='thrust_vector_horizontal_relay',
+        executable='thrust_vector_horizontal_relay',
+        package='sam_thruster_relay',
+        parameters=[config]
+    )
+
+    return LaunchDescription([
+        thrust_cmd_relay,
+        thrust_vector_relay,
+        thrust_vector_horizontal_relay
+    ])
