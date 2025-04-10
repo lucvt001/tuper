bff4528bd1030e361ce0928c8584e68d4650ea03
Luc <luc001@e.ntu.edu.sg>
2025-04-10 08:13:28 +0000

    Change to docker

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -2,8 +2,6 @@ docker run -it \
     --name tuper_container \
     --network=host \
     --ipc=host \
-    --env="DISPLAY" \
-    --env="QT_X11_NO_MITSHM=1" \
-    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+    -v /dev/shm:/dev/shm \
     -v $PWD:/home/smarc2user/colcon_ws \
     tuper:latest
